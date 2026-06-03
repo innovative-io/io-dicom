52090d1afe0fbf9069b39edae8d97214c6092c37
DeYung Le <deyung@gmail.com>
2026-06-02 19:57:35 -0500

    feat: expose public version package for external consumers

---
+++ version/version.go
@@ -0,0 +1,7 @@
+// Package version exposes the current release version of the io-dicom library.
+package version
+
+import internal "github.com/innovative-io/io-dicom/internal/version"
+
+// Version is the current release version of the io-dicom library.
+const Version = internal.Version
